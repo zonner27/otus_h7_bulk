b154cd1336015203ae2fb3a3a0666609d3544c9a
zonner27 <zonner2785@gmail.com>
2019-12-24 18:56:19 +0300

    work

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,87 @@
 #include <ctime>
 #include <iomanip>
 
+#define UNUSED(expr) do { (void)(expr); } while (0)
+
 class Packet;
 
 class Observer
 {
 public:
-    virtual void update(std::string str) = 0;
+    virtual void update(std::string str_commands, std::string str_time) = 0;
 };
 
 class Packet
 {
     std::vector<Observer *> subs;
     std::vector<std::string> commands;
+    std::string str_time;
+    int number_commnads = 2;
+    int m_iter = 0;
+    int m_bracket = 0;
+    std::string m_res_str = "bulk: ";
+
+    void set_time() {
+        std::chrono::time_point<std::chrono::system_clock> start_time;
+        start_time = std::chrono::system_clock::now();
+        std::time_t time_t = std::chrono::system_clock::to_time_t(start_time);
+        str_time = std::to_string(time_t);
+    }
 
 public:
+
+    void set_number(int _number) {
+        number_commnads = _number;
+    }
+
     void setstring(std::string str) {
-        commands.push_back(str);
+
+        if (str_time.size() == 0 && str != "{" && str != "}") {
+            set_time();
+        }
+
+        if (str == "{") {
+            m_bracket += 1;
+            if (m_res_str.size() > 6 && m_bracket == 1)
+                add_string();
+        } else if (str == "}") {
+            if (m_bracket > 0)
+                m_bracket -= 1;
+            if (m_bracket == 0 && m_res_str.size() > 6)
+                add_string();
+        } else {
+            if (m_bracket == 0) {
+                if (m_iter != (number_commnads - 1) ) {
+                    if (m_res_str.size() == 6) {
+                        m_res_str = m_res_str + str;
+                        ++m_iter;
+                    } else {
+                        m_res_str = m_res_str + ", " + str;
+                        ++m_iter;
+                    }
+                }
+                else {
+                    m_res_str = m_res_str + ", " + str;
+                    add_string();
+                }
+            } else {
+                if (m_res_str.size() == 6)
+                    m_res_str = m_res_str + str;
+                else
+                    m_res_str = m_res_str + ", " + str;
+            }
+        }
+    }
+
+    void add_string() {
+        commands.push_back(m_res_str);
+        m_iter = 0;
+        m_res_str = "bulk: ";
+    }
+
+    void modul() {
+        if (m_res_str.size() > 6 && m_bracket == 0)
+            add_string();
     }
 
     void subscribe(Observer *obs) {
@@ -29,10 +94,20 @@ public:
 
     void notify() {     
         for (auto s : subs) {
-            for (auto str: commands)
-                s->update(str);
+            for (auto str_commands: commands) {
+//                std::string result;
+//                for (int i = 0; i < number_commnads; ++i)
+//                {
+//                    result = "bulk: " + str_commands;
+//                }
+
+
+                s->update(str_commands, str_time);
+            }
         }
     }
+
+
 };
 
 class Cmd_Observer : public Observer
@@ -42,13 +117,9 @@ public:
         pack->subscribe(this);
     }
 
-    void update(std::string str) override {
-        std::cout << str;
-//        for (auto var : pack->commands)
-//        {
-//            std::cout << var << ", ";
-//        }
-        std::cout << std::endl;
+    void update(std::string str_commands, std::string str_time) override {
+        UNUSED(str_time);
+        std::cout << str_commands << std::endl;
     }
 };
 
@@ -59,12 +130,10 @@ public:
         pack->subscribe(this);
     }
 
-    void update(std::string str) override {
-        std::cout << str << ", ";
-//        for (auto var : pack->commands)
-//        {
-//            std::cout << var << ", ";
-//        }
+    void update(std::string str_commands, std::string str_time) override {
+        UNUSED(str_commands);
+        UNUSED(str_time);
+ //       std::cout << "\n" << str_commands << ", " << str_time << std::endl;
     }
 };
 
@@ -76,9 +145,9 @@ int main(int argc, char *argv[])
             std::cout << "Wrong amount arguments" << std::endl;
             return 1;
         }
-        std::cout << argv[1] << std::endl;
 
         Packet pack;
+        pack.set_number(atoi(argv[1]));
         Cmd_Observer cmdob(&pack);
         File_Observer fileobs(&pack);
 
@@ -86,23 +155,10 @@ int main(int argc, char *argv[])
         {
             pack.setstring(line);
         }
-
+        pack.modul();
         pack.notify();
 
 
-        std::chrono::time_point<std::chrono::system_clock> start_time;
-        start_time = std::chrono::system_clock::now();
-        std::time_t time_t = std::chrono::system_clock::to_time_t(start_time);
-        std::string time_command = std::to_string(time_t);
-        std::cout << time_command << std::endl;
-
-//        std::cout << std::to_string(m_time) << std::endl;
-
-
-
-
-        //    report_observer rpt(&lang);
-        //    ui_observer ui(&lang);
 
 //        std::vector<std::string> str;
 
